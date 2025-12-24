b534f4d96242be62fecf88d56acfe9dceff23748
16lazzy <fankaop@yandex.ru>
2025-12-24 20:24:49 +0300

    edited home page, fixed view core

--- core/views.py
+++ core/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.template.response import TemplateResponse
 from django.views.generic import TemplateView,DetailView
-
+from habits.models import Habit
 class IndexView(TemplateView):
-    template_name = 'core/base.html'
+    template_name = 'core/index.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-#/////////////////////
+        habits = Habit.objects.all().prefetch_related('logs')
+        context['habits'] = habits
         return context
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         if request.headers.get('HX-Request'):
             return TemplateResponse(request, 'core/index.html', context)
-        return TemplateResponse(request, 'core/base.html', context)
-
+        return self.render_to_response(context)
--- habits/models.py
+++ habits/models.py
@@ -4,6 +4,7 @@ class Habit(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000, blank=True)
     progress_mark = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Habitlog(models.Model):
     name = models.ForeignKey("Habit",on_delete=models.CASCADE, related_name='logs')
